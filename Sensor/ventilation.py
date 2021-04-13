435d28d32bb5579e3dc82af9693b9a782f67bb17
Sam <sam.fournier1212@gmail.com>
2021-04-13 19:45:10 -0400

    ajouter la classe motor

---
+++ Sensor/ventilation.py
@@ -0,0 +1,24 @@
+from gpiozero import Device, Motor, OutputDevice
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+class ventilation:
+    # defining variables
+    MOTOR_PINS = [23, 24]
+    ENABLE_PIN = 18
+    motor = None
+    
+    
+    def __init__(self):
+        self.motor = Motor(forward=self.MOTOR_PINS[1], backward=self.MOTOR_PINS[0],enable=self.ENABLE_PIN,pwm=True)
+        self.motor.stop()
+        pass
+    
+    def move(self,speed=0):
+        if(speed==0):
+            self.motor.stop()
+        elif(speed >0):
+            self.motor.forward(speed)
+        elif(speed < 0):
+            speed = speed*-1
+            self.motor.backward(speed)
+    
